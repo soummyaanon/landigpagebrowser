eee91196c7ef0ce0b00b26a49235dbdceafdd851
soummyaanon <soumyaranjanpanda910@gmail.com>
2025-05-12 17:12:00 +0530

    Add onClose prop to MobileNavMenu for better control

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -47,6 +47,7 @@ interface MobileNavMenuProps {
   children: React.ReactNode;
   className?: string;
   isOpen: boolean;
+  onClose?: () => void;
 }
 
 export const Navbar = ({ children, className }: NavbarProps) => {
@@ -195,6 +196,7 @@ export const MobileNavMenu = ({
   children,
   className,
   isOpen,
+  onClose,
 }: MobileNavMenuProps) => {
   return (
     <AnimatePresence>
